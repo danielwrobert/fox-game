26a84447bf6b60d935b35f563628169297e10d8f
Daniel W. Robert <danielwrobert@gmail.com>
2020-07-15 14:19:53 -0400

    Complete lessons 3 & 4 of States

--- src/constants.js
+++ src/constants.js
@@ -2,3 +2,12 @@ export const ICONS = ["fish", "poop", "weather"];
 export const TICK_RATE = 3000;
 export const RAIN_CHANCE = 0.2; // set to rain 20% of the time
 export const SCENES = ["day", "rain"];
+export const DAY_LENGTH = 60;
+export const NIGHT_LENGTH = 4;
+
+export const getNextHungerTime = (clock) =>
+  Math.floor(Math.random() * 3) + 5 + clock;
+export const getNextDieTime = (clock) =>
+  Math.floor(Math.random() * 2) + 3 + clock;
+export const getNextPoopTime = (clock) =>
+  Math.floor(Math.random() * 3) + 4 + clock;
--- src/gameState.js
+++ src/gameState.js
@@ -1,10 +1,23 @@
 import { modFox, modScene } from "./ui";
-import { SCENES, RAIN_CHANCE } from "./constants";
+import {
+  SCENES,
+  RAIN_CHANCE,
+  DAY_LENGTH,
+  NIGHT_LENGTH,
+  getNextHungerTime,
+  getNextDieTime,
+  getNextPoopTime,
+} from "./constants";
 
 const gameState = {
   current: "INIT",
   clock: 1,
   wakeTime: -1, // using -1 is referred to as a "sentinel value", which essentially means that it's not currently active. You could also use `undefined` but it's a good habit to keep things in the same Type.
+  sleepTime: -1,
+  hungryTime: -1,
+  dieTime: -1,
+  timeToStartCelebrating: -1,
+  timeToEndCelebrating: -1,
   tick() {
     this.clock++;
 
@@ -12,6 +25,16 @@ const gameState = {
 
     if (this.clock === this.wakeTime) {
       this.wake();
+    } else if (this.clock === this.sleepTime) {
+      this.sleep();
+    } else if (this.clock === this.hungryTime) {
+      this.getHungry();
+    } else if (this.clock === this.dieTime) {
+      this.die();
+    } else if (this.clock === this.timeToStartCelebrating) {
+      this.startCelebrating();
+    } else if (this.clock === this.timeToEndCelebrating) {
+      this.endCelebrating();
     }
 
     return this.clock;
@@ -25,9 +48,46 @@ const gameState = {
   wake() {
     this.current = "IDLING";
     this.wakeTime = -1;
-    modFox("idling");
     this.scene = Math.random() > RAIN_CHANCE ? 0 : 1;
     modScene(SCENES[this.scene]);
+    this.sleepTime = this.clock + DAY_LENGTH;
+    this.hungryTime = getNextHungerTime(this.clock);
+    this.determineFoxState();
+  },
+  sleep() {
+    this.state = "SLEEP";
+    modFox("sleep");
+    modScene("night");
+    this.wakeTime = this.clock + NIGHT_LENGTH;
+  },
+  getHungry() {
+    this.current = "HUNGRY";
+    this.dieTime = getNextDieTime(this.clock);
+    this.hungryTime = -1;
+    modFox("hungry");
+  },
+  die() {
+    console.log("die");
+  },
+  startCelebrating() {
+    this.current = "CELEBRATING";
+    modFox("celebrate");
+    this.timeToStartCelebrating = -1;
+    this.timeToEndCelebrating = this.clock + 2;
+  },
+  endCelebrating() {
+    this.timeToEndCelebrating = -1;
+    this.current = "IDLING";
+    this.determineFoxState();
+  },
+  determineFoxState() {
+    if (this.current === "IDLING") {
+      if (SCENES[this.scene] === "rain") {
+        modFox("rain");
+      } else {
+        modFox("idling");
+      }
+    }
   },
   handleUserAction(icon) {
     console.log(icon);
@@ -59,13 +119,24 @@ const gameState = {
     }
   },
   changeWeather() {
-    console.log("changeWeather");
+    this.scene = (1 + this.scene) % SCENES.length;
+    modScene(SCENES[this.scene]);
+    this.determineFoxState();
   },
   cleanUpPoop() {
     console.log("cleanUpPoop");
   },
   feed() {
-    console.log("feed");
+    // can only feed when hungry
+    if (this.current !== "HUNGRY") {
+      return;
+    }
+
+    this.current = "FEEDING";
+    this.dieTime = -1;
+    this.poopTime = getNextPoopTime(this.clock);
+    modFox("eating");
+    this.timeToStartCelebrating = this.clock + 2;
   },
 };
 
